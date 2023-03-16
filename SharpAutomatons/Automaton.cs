599ba3f1639a020abd0bce005727210952f85695
Radovan Blaha <kedar.blaha@gmail.com>
2023-03-16 16:09:25 +0100

    Added simple Automaton class

---
+++ SharpAutomatons/Automaton.cs
@@ -0,0 +1,69 @@
+﻿namespace SharpAutomatons;
+
+public class Automaton
+{
+    private readonly string _name;
+    private readonly State _initialState;
+    
+    
+    protected Automaton(string name, State initialState)
+    {
+        _name = name;
+        _initialState = initialState;
+    }
+
+
+    public class AutomatonBuilder
+    {
+        
+        private string _automatonName;
+        private State _initialState;
+        private HashSet<State> _states = new HashSet<State>();
+        
+        public AutomatonBuilder(string automatonName)
+        {
+            _automatonName = automatonName;
+        }
+        
+        public AutomatonBuilder InitialState(State initialState)
+        {
+            _initialState = initialState;
+            return this;
+        }
+        
+        public AutomatonBuilder AddState(string from, string to, char symbol)
+        {
+            State CreateStateIfNeeded(string target)
+            {
+                var state = _states.FirstOrDefault(s => s.Name == target);
+                if (state != null) return state;
+                
+                state = new State(target);
+                _states.Add(state);
+
+                return state;
+            }
+
+            var fromState = CreateStateIfNeeded(from);
+            
+            var toState = CreateStateIfNeeded(to);
+            fromState.AddTransition(symbol, toState);
+            
+            return this;
+        }
+        
+        public AutomatonBuilder RemoveState(State state)
+        {
+            _states.Remove(state);
+            return this;
+        }
+        
+        public Automaton Build()
+        {
+            return new Automaton(_automatonName, _initialState);
+        }
+
+
+    }
+
+}
---
+++ SharpAutomatons/State.cs
@@ -0,0 +1,27 @@
+﻿namespace SharpAutomatons;
+
+public class State
+{
+    private readonly string _name;
+    private readonly bool _isFinal;
+    private readonly Dictionary<char, HashSet<State>> _transitions = new Dictionary<char, HashSet<State>>();
+
+    public State(string name, bool isFinal = false)
+    {
+        _name = name;
+        _isFinal = isFinal;
+    }
+
+    public string Name => _name;
+
+    public bool IsFinal => _isFinal;
+    
+    public void AddTransition(char symbol, State to)
+    {
+        if (!_transitions.ContainsKey(symbol))
+        {
+            _transitions[symbol] = new HashSet<State>();
+        }
+        _transitions[symbol].Add(to);
+    }
+}
