ed401c7c4cf5923738ecd1272ef32994f14ecfe4
Radovan Blaha <kedar.blaha@gmail.com>
2023-03-16 19:23:18 +0000

    Added demo

--- SharpAutomatons/Program.cs
+++ SharpAutomatons/Program.cs
@@ -5,6 +5,24 @@ static class Program
 
     public static int Main()
     {
+
+        var automaton = new AutomatonBuilder("Test")
+            .AddState("S")
+            .AddState("A")
+            .AddState("B")
+            .AddTransition("S", "A", 'a')
+            .AddTransition("S", "B", 'b')
+            .AddTransition("A", "B", 'b')
+            .AddTransition("B", "B", 'b')
+            .MakeFinal("B")
+            .InitialState("S")
+            .Build();
+        
+        Console.WriteLine(automaton.Test("abb"));
+        Console.WriteLine(automaton.Test("b"));
+        Console.WriteLine(automaton.Test("a"));
+        Console.WriteLine(automaton.Test("ba"));
+        
         return 0;
     }
     
